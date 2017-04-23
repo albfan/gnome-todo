fb04de463a0893573b0af29363f3eaa31f346659
albfan <albertofanjul@gmail.com>
2017-04-23 09:05:12 +0200

    Fix test suite

--- tests/test-gtd-simple-test.c
+++ tests/test-gtd-simple-test.c
@@ -15,7 +15,7 @@ main (int   argc,
       char *argv[])
 {
   // initialize test program
-  gtk_test_init (&argc, &argv);
+  g_test_init (&argc, &argv);
   // hook up your test functions
   g_test_add_func ("/Simple Test Case", simple_test_case);
   // run tests from the suite
